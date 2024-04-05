8bb4d1a87436772c098203fff290d4842d13685e
DoaaMohamd <mrxpop7@gmail.com>
2024-04-05 06:01:52 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 'use client';
+import React, { Suspense  } from "react";
 import Image from "next/image";
-import styles from "./Page.module.css";
-import Navbar from "./component/navbar/navbar";
-import Hero from "./component/hero/Hero";
-import Serv from "./component/services/Serv";
-import Portofilio from "./component/portofilio/Portofilio";
-import Contact from "./component/contact/Contact";
-import Cursour from "./component/cursore/Cursour";
-import Paralax from "./component/paralax/Paralax";
+import styles from "./page.module.css";
+ 
+//import Hero from "./component/hero/Hero";
+//import Serv from "./component/services/Serv";
+//import Portofilio from "./component/portofilio/Portofilio";
+//import Contact from "./component/contact/Contact";
+// import Cursour from "./component/cursore/Cursour";
+//import Paralax from "./component/paralax/Paralax";
+const Navbar=React.lazy(()=> import  ("./component/navbar/navbar") ); 
+const Hero=React.lazy(()=> import  ("./component/hero/Hero") ); 
+const Serv=React.lazy(()=> import  ("./component/services/Serv") ); 
+const Portofilio=React.lazy(()=> import  ("./component/portofilio/Portofilio") ); 
+const Contact=React.lazy(()=> import  ("./component/contact/Contact") ); 
+const Paralax=React.lazy(()=> import  ("./component/paralax/Paralax") ); 
+const Cursour=React.lazy(()=> import  ("./component/cursore/Cursour") ); 
+
+
+
+
+
+
 
  function Home() {
  
@@ -15,6 +29,8 @@ import Paralax from "./component/paralax/Paralax";
      
     
     <div>
+      <></>
+      <React.Suspense fallback="Loading...">
     <Cursour/>
 
     <section className={styles.sec}id ="Homepage"><Navbar/>
@@ -28,7 +44,7 @@ import Paralax from "./component/paralax/Paralax";
 
 
 
-
+    </React.Suspense>
     </div>
      
   );}
